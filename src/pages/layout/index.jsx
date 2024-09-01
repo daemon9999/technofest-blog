8bc5ae4a2a9a7c97b8110917fd55ba306f6c2e68
Murad Masimli <murad.mesimli.99@gmail.com>
2024-09-01 05:49:29 +0400

    feature: scroll

--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -1,11 +1,17 @@
+import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Menu from "~/components/menu";
 import SiteProvider, { useSiteContext } from "~/context";
 import Footer from "~/layouts/footer";
 import Header from "~/layouts/header";
 
 export default function Layout() {
+    const location = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0,0)
+    }, [location.pathname])
   return (
   <>
         <SiteProvider>
